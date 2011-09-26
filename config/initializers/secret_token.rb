e958eaaae1407e4fbe651ed920fd727f457ab0a8
Krzysztof Modras <slain@yesod.(none)>
2011-09-26 18:42:22 +0200

    bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MetabraneCo::Application.config.secret_token = '8c6755bc154667b47b7a56e05cf914d8ce2ac93e32afc0d9f7108aeb24c73670057592fec4b804a54b500a53d3087aa3d5fb9d48d4e6849267062b9b68f7a8a9'
