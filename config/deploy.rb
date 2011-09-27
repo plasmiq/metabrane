91329ba6ffa640a343b5fe4d0cdd9a7b2d6d0746
Krzysztof Modras <slain@yesod.(none)>
2011-09-27 10:09:46 +0200

    Capistrano: final config (?)

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,17 +18,23 @@ role :db,  '178.63.75.143', :primary => true
 
 desc 'Symlinks the :app_symlinks'
   task :after_update_code do
-    app_symlinks.each do |link|
+  app_symlinks.each do |link|
     run "ln -nfs #{shared_path}/#{link} #{release_path}/#{link}"
   end
 end
 
+desc "precompile the assets"
+task :precompile_assets, :roles => :web, :except => { :no_release => true } do
+  run "cd #{current_path}; rm -rf public/assets/*"
+  run "cd #{current_path}; RAILS_ENV=production bundle exec rake assets:precompile"
+end
+
 namespace :deploy do
   task :start, :roles => :app, :except => { :no_release => true } do
-    run "cd #{current_path} && bundle exec passenger start --socket tmp/passenger.socket --daemonize --environment production -p 13001"
+    run "cd #{current_path} && bundle exec passenger start --daemonize --environment production --port 13001"
   end
   task :stop, :roles => :app, :except => { :no_release => true } do
-    run "cd #{current_path} && bundle exec passenger stop --pid-file tmp/pids/passenger.pid"
+    run "cd #{current_path} && bundle exec passenger stop --pid-file tmp/pids/passenger.13001.pid"
   end
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path, 'tmp', 'restart.txt')}"
