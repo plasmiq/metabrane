452d47fa48ad26f57a6553dc047eeea998bbf061
Krzysztof Modras <slain@yesod.(none)>
2011-10-02 16:42:22 +0200

    Substrate: image scaling for less data send to webbrowser
    WARNING: need to reprocess all images after deployment. USE:
    Substrate.all.each{ |s| s.image.reprocess! }

--- app/models/substrate.rb
+++ app/models/substrate.rb
@@ -3,7 +3,8 @@ require "open-uri"
 class Substrate < ActiveRecord::Base
   has_many :working_pair
   
-  has_attached_file :image
+  has_attached_file :image,
+    :styles => { :poster => "229x" }
 
   def picture_from_url(url)
     self.image = open(url)
