5a41d1c33c208228352f385a86883184d2d5facf
Krzysztof Modras <slain@yesod.(none)>
2011-10-22 12:37:12 +0200

    refactoring: weave options

--- app/helpers/weaves_helper.rb
+++ app/helpers/weaves_helper.rb
@@ -5,13 +5,39 @@ module WeavesHelper
       content_tag( :span, weave.status_name, :class => "status" )  + timeago
   end 
   
+  def weave_options_tag weave, options = {}
+    html = weave_timestamp(weave)
+    toggles = ""
+    toggles += weave_delete_link(weave) if options[:delete]
+    toggles += weave_fav_link(weave) if options[:fav]
+    toggles += weave_permalink(weave) if options[:permalink]
+    html += content_tag :div, raw(toggles), :class => "toggles"
+
+    content_tag( :div, raw(html), :class => "timestamp")
+  end
+  
   def weave_fav_link weave
     link_to '', 
         {:controller => :weaves, :action => :favorite, :id => weave.id},
         :method =>  :post,
         :remote => true,
         :taget => '_blank',
-        :class=> "favorite_weave favorite_#{weave.favorite?} favorite_#{weave.id}"
-        
+        :class=> "favorite_weave favorite_#{weave.favorite?} favorite_#{weave.id}"      
+  end
+  
+  def weave_permalink weave
+    link_to '', 
+        {:controller => :weaves, :action => :show, :id => weave.id},
+        :taget => "_blank",
+        :class=>'permalink_weave' 
+  end
+  
+  def weave_delete_link weave 
+    link_to '', 
+        {:controller => :weaves, :action => :destroy, :id => weave.id}, 
+        :confirm => 'Are you sure?', 
+        :method => :delete, 
+        :remote=> true, 
+        :class=>'delete_weave'
   end
 end
