7030df80bb203c62d63975ab715dcb3673e16673
Krzysztof Modras <slain@yesod.(none)>
2011-11-12 17:16:34 +0100

    working_pairs refactorization

--- app/models/working_pair.rb
+++ app/models/working_pair.rb
@@ -1,21 +1,36 @@
 class WorkingPair < ActiveRecord::Base
   belongs_to :substrate1,:class_name => "Substrate", :foreign_key => "substrate1_id"
   belongs_to :substrate2,:class_name => "Substrate", :foreign_key => "substrate2_id"
-  
-  scope :recent, order("working_pairs.updated_at DESC")
-  scope :favorited, 
-    lambda { includes(:favorites).where("favorites.working_pair_id = working_pairs.id") }
-  
   has_many :favorites
   
   validates :relation, :presence => true
   
+  scope :recent, order("working_pairs.updated_at DESC")
+  scope :favorited, lambda { 
+    includes(:favorites)
+    .where("favorites.working_pair_id = working_pairs.id") 
+  }
+  
+  scope :same_substrates, lambda { |substrate1, substrate2|
+    where( 
+      "((substrate1_id = ?) and (substrate2_id = ?)) or ((substrate1_id = ?) and (substrate2_id = ?))",
+      substrate1.id, substrate2.id, substrate2.id, substrate1.id
+    )
+  }
+  
+  scope :entangled, lambda { |metatag,*substrates|
+    t = WorkingPair.arel_table
+    where( 
+      t[:relation].eq( metatag )
+      .or( t[:substrate1_id].in( substrates ) )
+      .or( t[:substrate2_id].in( substrates ) ) 
+    )
+  }
   
   CREATED    = 1
   EXTENDED   = 2
   TWEAKED    = 3
-
-
+  
   STATUSES = {
     CREATED   => 'created',
     EXTENDED  => 'extended',
@@ -31,14 +46,12 @@ class WorkingPair < ActiveRecord::Base
     favorites.count > 0
   end
   
-  def entangeled_weaves
-    cond = "((substrate1_id = ?) and (substrate2_id = ?)) or ((substrate1_id = ?) and (substrate2_id = ?))"
-    
-    WorkingPair.where( cond, self.substrate1_id_before_type_cast, self.substrate2_id, self.substrate2_id, self.substrate1_id ).order("updated_at DESC")
+  def weaves_with_same_substrates
+    WorkingPair.same_substrates(substrate1, substrate2).order("updated_at DESC")
   end
   
   def entangled
-    WorkingPair.where( "relation = ? or (substrate1_id = ? or substrate2_id = ?) or (substrate1_id = ? or substrate2_id = ?)", relation, substrate1.id, substrate2.id, substrate2.id, substrate1.id)
+    WorkingPair.entangled(relation, substrate1.id, substrate2.id)
   end
   
   def older 
