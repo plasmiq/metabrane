47615f75aaf52418fec011e98eb0025ae10f02af
Krzysztof Modras <slain@yesod.(none)>
2011-11-15 00:11:51 +0100

    display subnav indication of scope of exploration for each metatag if loaded into top metatag as home

---
+++ app/helpers/subnav_helper.rb
@@ -0,0 +1,26 @@
+module SubnavHelper
+  def distance_from_home metatags, metatag, home
+    dist = (- metatags.index(metatag) + metatags.index(home) )
+    format_distance dist
+  end
+  
+  def format_distance dist
+    if dist > 0
+      dist = "+"+dist.to_s 
+    elsif dist == 0
+      dist = raw('&nbsp;'+dist.to_s) 
+    else
+      dist = dist.to_s if dist < 0
+    end
+  end
+  
+  def weave_potential_tag weave
+    html =  content_tag( :div, 
+      content_tag( :span, format_distance( weave.older.all.size ) ), 
+      :class => "older")
+    html += content_tag( :div, 
+      content_tag( :span, format_distance( weave.newer.all.size ) ), 
+      :class => "newer")
+    content_tag( :div, raw(html), :class => "potential")
+  end
+end
--- app/helpers/substrates_helper.rb
+++ app/helpers/substrates_helper.rb
@@ -1,13 +1,3 @@
 module SubstratesHelper
-  def distance_from_home metatags, metatag, home
-    dist = (- metatags.index(metatag) + metatags.index(home) )
-    if dist > 0
-      dist = "+"+dist.to_s 
-    elsif dist == 0
-      dist = raw('&nbsp;'+dist.to_s) 
-    else
-      dist = dist.to_s if dist < 0
-    end
-    dist
-  end
+  
 end
