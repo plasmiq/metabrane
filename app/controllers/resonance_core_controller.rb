f07b3c6de6f5d7b378bcac57191ea50b5b8c5879
Artur Małecki <artur.malecki@aenima.pl>
2011-12-17 00:48:24 +0100

    Removed authentication for resonance core

--- app/controllers/resonance_core_controller.rb
+++ app/controllers/resonance_core_controller.rb
@@ -1,4 +1,6 @@
 class ResonanceCoreController < ApplicationController
+  skip_before_filter :authentication_check
+
   def bind
     render :text => Substrate.order('RAND()').first.image.url(:poster)
   end
