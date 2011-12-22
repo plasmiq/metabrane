35c864018f49f7fd5401339bf6f1d73ffc543607
Artur Małecki <artur.malecki@aenima.pl>
2011-12-22 23:13:17 +0100

    Resonance core - add json format to response'

--- app/controllers/resonance_core_controller.rb
+++ app/controllers/resonance_core_controller.rb
@@ -2,6 +2,6 @@ class ResonanceCoreController < ApplicationController
   skip_before_filter :authentication_check
 
   def bind
-    render :text => Substrate.order('RAND()').first.image.url(:poster)
+    render {:url => Substrate.order('RAND()').first.image.url(:poster)}.to_json
   end
 end
