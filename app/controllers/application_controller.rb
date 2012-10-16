f0ebf222a1169862759f14af2d329862b2fe30b7
Krzysztof Modras <krzysztof.modras@gmail.com>
2012-10-16 14:05:38 +0200

    remove simple http auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,8 +16,8 @@ class ApplicationController < ActionController::Base
  
   private
     def authentication_check
-      authenticate_or_request_with_http_basic do |user, password|
-        user == USER && password == PASSWORD
-      end
+      #authenticate_or_request_with_http_basic do |user, password|
+      #  user == USER && password == PASSWORD
+      #end
     end
 end
